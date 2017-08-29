87399834e47216f12294983f11d92f3ca0299c6a
Matt Kelsey <mattkelseymk@gmail.com>
2017-08-28 17:32:06 -0700

    add imu and movment classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Movement.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.adafruit.AdafruitBNO055IMU;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by matt on 8/28/17.
+ */
+
+public class Movement {
+    Movement(AdafruitBNO055IMU imu, DcMotor m0, DcMotor m1) {
+
+    }
+}
